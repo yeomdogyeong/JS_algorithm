8981fcab0ae510622de7e8790faddf3f9affb6dd
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-09-13 00:50:47 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,11 +1,7 @@
 function solution(str1, str2) {
     let answer = ''
 for(let i =0; i < str1.length; i++){
-    if(str1.length === str2.length){
     answer += str1[i] + str2[i]
-} 
 }
-
-
     return answer
 }
