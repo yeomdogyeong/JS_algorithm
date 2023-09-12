e44bb2b94317cbac7f6f74d2218f8df92476c5e3
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-09-13 02:00:06 +0900

    [unrated] Title: 배열 조각하기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+    for(let i = 0; i< query.length; i++){
+        if(i % 2 === 0){
+            arr.splice(query[i] + 1)
+        }else if(i % 2 === 1){
+            arr.splice(0, query[i])
+        }
+    }
+    return arr
+}
