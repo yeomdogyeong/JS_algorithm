f1367ccce09acdd298146f320a311fab690a20cb
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-09-20 22:57:27 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,6 @@
+function solution(a, b, c) {
+      if(a === b && b === c){
+          return (a + b + c) * ((a**2) + (b**2) + (c**2) ) * ((a**3) + (b**3) + (c**3) )
+      }else if (a !== b && b!== c && c !==a)return a + b + c
+  else return  (a + b + c) * ((a**2) + (b**2) + (c**2) )
+}
