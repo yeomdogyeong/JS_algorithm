c797c215a699c5ae4298642a4b825691bff0d00e
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-10-06 02:19:19 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,14 +1,16 @@
 function solution(rank, attendance) {
     let arr = []
   for(let i =0 ; i < attendance.length; i++){
-      if(attendance[i]){
+      if(attendance[i] === true){
           //boolean값이 true인 index를 추출한 배열
-          arr.push(rank[i])
+          arr.push(i)
       }
   }
-    arr.sort((a,b) => a - b)
-const answer = (i) => {
-    return rank.indexOf(arr[i])
-}
-  return 10000 * answer(0) + 100 * answer(1) + answer(2);
+  const result = arr.map(i => rank[i]).sort().slice(0,3)
+  
+    
+  const answer = result.map(el => rank.indexOf(el))
+ 
+    return 10000 * answer[0] + 100 * answer[1] + answer[2] 
+  
 }
