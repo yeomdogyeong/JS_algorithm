0af36843642bca61445b426391433200919c374c
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-10-12 14:36:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 15.88 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{
+   let answer = []
+   for(let i= 0 ; i< arr.length; i++){
+   if(i === 0 || arr[i] !== arr[i-1]){
+       answer.push(arr[i])
+   }
+   }
+    return answer
+}
