590c964935e4cac74cbccada6543b5896710357c
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-10-16 22:13:16 +0900

    [level 1] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,17 @@
+function solution(name, yearning, photo) {
+    let result = []
+  for(let i = 0 ; i< photo.length; i++){
+      let answer = 0
+    for(let j = 0; j< photo[i].length; j++){
+        let index = name.indexOf(photo[i][j])
+        if(index !== -1){
+            answer += yearning[index]
+        }
+    }
+      result.push(answer)
+  }
+    return result
+}
+
+//name배열에 있는 el들이 photo 배열 안의 배열 요소라면, name의 index값을 yearning의 index값에 적용시키고
+//(해당 index들의 합)을 result배열의 el로 적용시킨다
