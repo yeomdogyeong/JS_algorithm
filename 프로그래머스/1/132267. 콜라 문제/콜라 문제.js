77ad391d270b929dc077729b652710932feea40a
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-10-18 00:59:53 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,10 @@
+function solution(a, b, n) {
+ 
+    let total = 0
+    while(n >= a){
+        let coke = Math.floor( n / a) * b
+        total += coke
+        n = coke + (n % a)
+    }
+    return total
+}
