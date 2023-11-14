8219152a59f34dad64d345d554f51d095e270afd
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-11-15 01:08:10 +0900

    [level unrated] Title: 컨트롤 제트, Time: 0.15 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,17 +1,13 @@
 function solution(s) {
-    let arr = s.split(' ')
-    let length = arr.length
-    let answer = 0
-    let minus = 0;
-    console.log(arr)
-   for(let i = 0; i < arr.length; i++){
-      
-       if(arr[i] === 'Z'){
-          answer -= minus;
+   let arr = s.split(' ')
+   let answer = 0
+   
+   arr.forEach((el,index) => {
+       if(el === 'Z'){
+           answer -= Number(arr[index-1])
        }else{
-            answer += arr[i]*1
-            minus = arr[i]*1
-        }
-   }
-    return answer;
+           answer += Number(arr[index])
+       }
+   })
+   return answer;
 }
