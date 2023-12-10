4c6a32be8acebd47d6bb3f2fe26393829438224a
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-12-11 02:19:36 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.48 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,16 @@
+function solution(food) {
+    let arr = []
+    let answer = ''
+  for(let i = 1; i<food.length; i++){
+   let mok = Math.floor(food[i] / 2)
+   console.log(mok)
+      for(let j = 0; j<mok; j++){
+          arr.push(i)
+      }
+  }
+    let string = arr.join('')
+   let rev = arr.reverse().join('')
+
+    answer += string + 0 + rev
+    return answer
+}
