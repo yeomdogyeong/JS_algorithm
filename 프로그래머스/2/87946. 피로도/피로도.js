a262a174695a8f965de479e6f747d71c4d580a56
Sia <82639552+yeomdogyeong@users.noreply.github.com>
2024-01-08 01:25:55 +0900

    [level 2] Title: 피로도, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,26 +1,11 @@
 function solution(k, dungeons) {
-    //도는 경로를 모은 배열
-    let answer = 0
-    //dungeon의 수만큼 나열되고 false로 채워진 배열생성
-    const visited = new Array(dungeons.length).fill(false)
-    
-    function dfs(hp, count){
-       
-        for(let i = 0 ; i<dungeons.length; i++){
-         
-            if(!visited[i] && dungeons[i][0] <= hp){
 
-                visited[i] = true;
-                dfs(hp-dungeons[i][1], count + 1)
-                visited[i] = false;
-          
-            }
-        }
-        answer = Math.max(answer, count)
+    let answer = 0
+    for(let i = 0; i<dungeons.length; i++){
+            if(dungeons[i][0] <= k){
+                k = k - dungeons[i][1]
+                answer++
+            }else continue;
     }
- 
-    dfs(k,0)
-    
-    return answer;
-    
+    return answer
 }
