7d6cf621dff2800b2900807a59250d5bff3e0a25
Sia <82639552+yeomdogyeong@users.noreply.github.com>
2024-02-18 22:40:29 +0900

    [level 1] Title: 달리기 경주, Time: 0.15 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+    const playersObj = {};
+    for(let i =0; i<players.length; i++){
+
+        playersObj[players[i]] = i;
+    }
+  
+    for(let i=0; i< callings.length; i++){
+        let index = playersObj[callings[i]]
+        let change = players[index-1]
+        //callings에서 나온 요소와 그 앞의 요소를 서로 바꿈
+        players[index-1] = callings[i]
+        players[index] = change;
+        //객체에서도 각자의 순서를 바꿔줌
+        playersObj[callings[i]] = index-1;
+        playersObj[change] = index;
+    }
+    return players;
+}
