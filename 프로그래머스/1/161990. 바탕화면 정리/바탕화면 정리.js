71741ddb79d878dd2a1d6d0c719ca25c058ca77b
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-03 16:39:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,16 @@
+   function solution(wallpaper) {
+    let minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
+
+    for (let i = 0; i < wallpaper.length; i++) {
+        for (let j = 0; j < wallpaper[i].length; j++) {
+            if (wallpaper[i][j] === '#') {
+                minX = Math.min(minX, i);
+                minY = Math.min(minY, j);
+                maxX = Math.max(maxX, i);
+                maxY = Math.max(maxY, j);
+            }
+        }
+    }
+
+    return [minX, minY, maxX + 1, maxY + 1];
+}
