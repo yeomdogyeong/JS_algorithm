560f169f1bd314a685da45f51584d6ec644b7a4a
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-05-09 23:45:24 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 0.13 ms
+메모리: 33.6 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 22:8:10
+2024년 05월 09일 23:45:21
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,19 +1,17 @@
 function solution(a, b) {
-    let answer = 0;
-    let small = 0;
-    let big = 0;
-    
-    if(a === b)return a;
-    
-    if(a < b){
-        small = a,
-        big = b
-    }else if(a > b){
-        small = b,
-        big = a
-    }
-    for(let i = small; i<=big; i++){
-        answer += i
-    }
-    return answer;
+   let result = 0
+
+if(a === b) return a;
+
+if(a < b){
+        for(let i = a; i<=b; i++){
+       result += i
+   }
+}else {
+            for(let i = b; i<=a; i++){
+       result += i
+   }
+}
+
+    return result;
 }
