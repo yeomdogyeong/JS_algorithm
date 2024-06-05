0c86f1ef06e6f440895196d0a4f6cdf1cf89715b
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-06-06 02:47:11 +0900

    [level 1] Title: 덧칠하기, Time: 1.32 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,16 +1,14 @@
 function solution(n, m, section) {
-   let arr = new Array(n).fill(1)
     let result = 0;
-    for(let i = 0; i<section.length; i++){
-      arr[section[i]-1] = 0
-    }
-    console.log(arr)
+    let part = 0;
     
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i] === 0){
-            result++
-            i = i + m - 1
-        }
-    }
-    return result;
+    section.forEach((item)=>{
+            if(item > part){
+        part = item + m - 1
+        result++
+    }})
+    return result; 
 }
+    
+                  
+                 
