771a1849ad42e8ea2d01f7d57e3ce899e3d33260
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-08 23:25:15 +0900

    [level 1] Title: 달리기 경주, Time: 403.96 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -11,7 +11,7 @@ function solution(players, callings) {
         //callings에서 나온 요소와 그 앞의 요소를 서로 바꿈
         players[index-1] = callings[i]
         players[index] = change;
-        //객체에서도 각자의 순서를 바꿔줌
+         //객체에서도 각자의 순서를 바꿔줌
         playersObj[callings[i]] = index-1;
         playersObj[change] = index;
     }
