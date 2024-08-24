6514323e4fa8ee193e3aa90ed35478652dbdf2fd
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-24 23:51:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 265.17 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -10,14 +10,10 @@ function solution(id_list, report, k) {
             obj[b] = {};
             obj[b][a] = true;
         }
-        
     }
- 
     for(let i = 0 ; i < id_list.length; i++){
         person[id_list[i]] = 0;
     }
-       
-
     for(let i = 0 ; i < id_list.length; i++){
         if(obj[id_list[i]]){
             const keys = Object.keys(obj[id_list[i]]);
