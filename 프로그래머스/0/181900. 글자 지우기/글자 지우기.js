88f760836808af1fc50c5874a31e766636c4fb53
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-25 21:13:38 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,6 +1,6 @@
 function solution(my_string, indices) {
     let my_string2 = my_string.split('')
-    //indices에 속하지 않는 index만 필터링
+    // indices에 속하지 않는 index만 필터링
     let filteredString = my_string2.filter((item, index) => !indices.includes(index))
     return filteredString.join('')
 }
