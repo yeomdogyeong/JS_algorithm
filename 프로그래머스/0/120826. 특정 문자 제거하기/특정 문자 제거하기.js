626908c9f463f65790353e4c014f7f569d1fbfc1
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-28 20:21:02 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, letter) {
+    let answer = ''
+    for(let i = 0; i<my_string.length; i++){
+        if(my_string[i] !== letter){
+            answer += my_string[i]
+        }
+    }
+    return answer;
+}
