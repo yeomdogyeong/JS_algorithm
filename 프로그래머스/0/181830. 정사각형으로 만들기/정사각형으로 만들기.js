b169b4cf5b11e0460b068abfaef109afe66355c4
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-29 22:45:57 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.57 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    const hang = arr[0].length
+    const yeul = [...arr].length
+   
+    if(hang === yeul) return arr
+  
+    if(hang > yeul){
+        for(let i = yeul; i<hang; i++){
+            const someArr = new Array(hang).fill(0);
+            arr.push(someArr)
+        }
+    }else{
+       for (let i = 0; i < arr.length; i++) { 
+            for(let j = 0; j<yeul-hang; j++){
+                 arr[i].push(0)
+            }
+           
+            }
+    }
+    return arr
+}
