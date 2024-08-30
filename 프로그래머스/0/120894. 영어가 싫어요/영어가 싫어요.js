32380b04021bf7019f50386eac11e8493a24c287
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-08-31 01:10:06 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+   let object = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'zero':0}
+ 
+   let result = ''
+  while(numbers.length > 0){
+      for(let key in object){
+          if(numbers.startsWith(key)){
+              result += object[key]
+              numbers = numbers.substring(key.length)
+          }
+      }
+  }
+  return parseInt(result)
+   
+}
