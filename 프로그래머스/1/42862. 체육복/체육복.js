61583cb2c2a9c2284202af44e4c1c10ad800fb47
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-02 18:58:48 +0900

    [level 1] Title: 체육복, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -10,7 +10,7 @@ function solution(n, lost, reserve) {
      if (reserve.includes(lost[i]-1)) {
             reserve.splice(reserve.indexOf(lost[i]-1), 1); 
             lost.splice(i,1); 
-            i -= 1 
+            i -= 1  
         } else if (reserve.includes(lost[i]+1)) {
             reserve.splice(reserve.indexOf(lost[i]+1), 1); 
             lost.splice(i,1); 
