bc07ade841dbe4fcdc79d490ea7a03d71fb16c37
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-20 13:16:11 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -0,0 +1,16 @@
+function solution(rank, attendance) {
+    let arr = []
+  for(let i =0 ; i < attendance.length; i++){
+      if(attendance[i] === true){
+          //boolean값이 true인 index를 추출한 배열
+          arr.push(i)
+      } 
+  }
+  const result = arr.map(i => rank[i]).sort((a,b) => a - b).slice(0,3)
+  
+    
+  const answer = result.map(el => rank.indexOf(el))
+ 
+    return 10000 * answer[0] + 100 * answer[1] + answer[2] 
+  
+}
