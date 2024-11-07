3006f63b5f83f7f6bfb74108c8567f85cc64aa7b
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-11-07 20:45:26 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.79 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,63 +1,32 @@
 function solution(today, terms, privacies) {
 
-let termsArray = []
-    for(let i=0; i<terms.length; i++){
-        let termsType = terms[i].split(' ')
-        termsArray.push([termsType[0], parseInt(termsType[1])]); // 숫자로 변환
+ let termsArray = {};
+    for (let term of terms) {
+        let [type, months] = term.split(' ');
+        termsArray[type] = parseInt(months);
     }
-     console.log(termsArray)
-let privaciesArray = []
-let privacyDate = []
-        for(let i=0; i<privacies.length; i++){
-        let privacyType = privacies[i].split(' ')
-         privacyDate.push(privacyType[0].split('.').map(Number)); // 날짜를 숫자 배열로 변환
-        privaciesArray.push(privacyType)
-    }
-     console.log(privaciesArray)
-    console.log(privacyDate)
-let middleResult = []
-    for(let i =0; i<privaciesArray.length; i++){
-        for(let j =0; j<termsArray.length; j++){
-            if(privaciesArray[i][1] === termsArray[j][0]){
-                middleResult.push(termsArray[j][1]); // 숫자로 저장된 개월 수를 추가
-            }
-        }
-    }
-    console.log(middleResult)
-    
-    //합친 날짜
-    for(let i =0; i<privacyDate.length; i++){
-        // if(privacyDate[i][1] + middleResult[i] > 12){
-        //     //크다면 
-        //     privacyDate[i][0] = privacyDate[i][0] + 1
-        //     privacyDate[i][1] = privacyDate[i][1] + middleResult[i] - 12
-        // }else{
-        //     //12랑 같거나 작다면
-        //     privacyDate[i][1] = privacyDate[i][1] + middleResult[i]
-        // }
-        privacyDate[i][1] += middleResult[i];
-        if (privacyDate[i][1] > 12) {
-            privacyDate[i][0] += Math.floor((privacyDate[i][1] - 1) / 12);
-            privacyDate[i][1] = (privacyDate[i][1] - 1) % 12 + 1;
-        }
-    }
+
     let todayArray = today.split('.').map(Number);
-    let result = []
-     for(let i =0; i< privacyDate.length; i++){
-    //     if(todayArray[i] <= privacyDate[i]){
-    //        continue;
-    //     }else if(todayArray[2] === privacyDate[2]){
-    //         result.push(i+1)
-    //     }else{
-    //         result.push(i+1)
-    //     }
-        if (
-            todayArray[0] > privacyDate[i][0] ||
-            (todayArray[0] === privacyDate[i][0] && todayArray[1] > privacyDate[i][1]) ||
-            (todayArray[0] === privacyDate[i][0] && todayArray[1] === privacyDate[i][1] && todayArray[2] >= privacyDate[i][2])
-        ) {
-            result.push(i + 1);
+    let result = [];
+
+    for (let i = 0; i < privacies.length; i++) {
+        let [date, type] = privacies[i].split(' ');
+        let [year, month, day] = date.split('.').map(Number);
+
+        // 유효기간을 더해 만료일 계산
+        month += termsArray[type];
+        while (month > 12) {
+            month -= 12;
+            year += 1;
+        }
+
+        // 만료일과 오늘 날짜 비교
+        if (year < todayArray[0] || 
+            (year === todayArray[0] && month < todayArray[1]) || 
+            (year === todayArray[0] && month === todayArray[1] && day <= todayArray[2])) {
+            result.push(i + 1); // 인덱스는 1부터 시작하므로 i+1
         }
     }
+
     return result;
 }
