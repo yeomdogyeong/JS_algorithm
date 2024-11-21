bba08bf2a8db43d129d0fd4080d58a74306df37a
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-11-21 20:10:06 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 23.07 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let su = "수"
+    let bak = "박" 
+    let answer = ''
+for(let i =0; i<n; i++)
+    if(i % 2 === 0){
+    answer += su
+    }else if(i % 2 === 1){
+    answer += bak
+    }
+return answer;
+}
