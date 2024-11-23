ddc8fd3d3d5a37aa8bef7e966a5df7808cdc281f
do_lly <82639552+yeomdogyeong@users.noreply.github.com>
2024-11-23 22:05:56 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.62 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -4,13 +4,19 @@ function solution(board, moves) {
    for(let i = 0; i< moves.length; i++){
        let number = moves[i]-1
        for(let j = 0; j < board.length; j++){
+         // board의 값중에 원하는 인덱스 중에서 0이 아닌 값을 발견했을때
            if(board[j][number] !== 0){
+             //내가 만든 배열에서 마지막 값이 해당 값과 일치한다면
                if(stack[stack.length-1] === board[j][number]){
+                 //값을 증가시키고
                    answer += 2
+                 //stack에서 해당 값을 제외한다
                    stack.pop()
-               }else {
+               }else { 
+                 //마지막 값이 일치하지 않는다면 stack배열에 집어넣는다.
                    stack.push(board[j][number])
                }
+             //0이 아닌 값을 찾았으니 해당 값을 0으로 만들고 반복문종료
                board[j][number] = 0;
                break;
            }
